2a27f2c4a966c9713698423235aa14f78fa24b41
Nekr000 <nekrcode000@gmail.com>
2022-02-03 19:21:16 +0300

    homeWork5

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './p1-main/m1-ui/u1-app/App'
-import * as serviceWorker from './serviceWorker'
+import App from './src/p1-main/m1-ui/u1-app/App'
+import * as serviceWorker from './src/./serviceWorker'
 
 ReactDOM.render(
     <React.StrictMode>
